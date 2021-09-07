80ed5d51056dd03aa0b1134956fed956253de1a9
wjhwjh <you@example.com>
2021-09-07 17:39:21 +0800

    模板

--- routers/router.js
+++ routers/router.js
@@ -4,13 +4,14 @@ router.get('/', async (ctx) => {
     //ctx.body = "首页";
     await ctx.render("index",{
         name:"huizi",
-        islogin:true
+        islogin:false
     })
 })
 
-router.get('/user', async (ctx) => {
+router.get('user/login', async (ctx) => {
    // await ctx.render("list")
-   ctx.body="{'data':'返回数据'}"
+   //ctx.body="{'data':'返回数据'}"
+   await ctx.render("component/reglogin");
 })
 
 module.exports = router
